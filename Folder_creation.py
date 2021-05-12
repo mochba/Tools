e5bf5a30e6561cd50d517e78d5f1f135d8102c22
zdiscov <zdiscov@gmail.com>
2021-05-11 19:19:46 -0700

    Moved import statements to the top

--- Folder_creation.py
+++ Folder_creation.py
@@ -1,3 +1,5 @@
+import gzip
+import shutil
 from pathlib import Path
 def create_folder(path):
     Path(path).mkdir(parents=True,exist_ok=True)
@@ -6,8 +8,6 @@ file = open("/home/appu/Documents/python_workbench/Roche/path.txt")
 for line in file:
     create_folder(line)    
 
-import gzip
-import shutil
 with gzip.open('file.txt.gz', 'rb') as f_in:
     with open('file.txt', 'wb') as f_out:
         shutil.copyfileobj(f_in, f_out)
